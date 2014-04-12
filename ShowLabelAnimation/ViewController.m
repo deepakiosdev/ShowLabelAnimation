ed85d9d3fb1355d68c005d13e29c4289e549c656
DeepakIosDev <deepak.262903@gmail.com>
2014-04-12 10:53:42 +0530

    Payment app

--- ShowLabelAnimation/ViewController.m
+++ ShowLabelAnimation/ViewController.m
@@ -9,19 +9,26 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+{
+    NSString *login;
+    NSString *password;
+}
+
 @property (weak, nonatomic) IBOutlet UILabel *score;
 @property (strong, nonatomic) NSTimer *timer;
+
 @end
 
 @implementation ViewController
 
 
 
-- (void)viewDidLoad
-{
+
+- (void)viewDidLoad {
+    
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
-     self.score.text = [NSString stringWithFormat:@"%d", 500];
+    self.score.text = [NSString stringWithFormat:@"%d", 500];
 }
 
 - (void) viewDidAppear:(BOOL)animated
@@ -35,7 +42,27 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex {
+    
+    if (alertView.tag == 0 && buttonIndex == 1) {
+        NSLog(@"Button 2 was pressed ,Login:%@,PWD:%@", [alertView textFieldAtIndex:0].text,[alertView textFieldAtIndex:1].text);
+        if ([[alertView textFieldAtIndex:0].text isEqualToString:login] && [[alertView textFieldAtIndex:1].text isEqualToString:password]) {
+            NSLog(@"Correct login and password");
+        }
+    }
+    
+}
+
 - (IBAction)scoreButtonPressed:(id)sender {
+    
+    login = @"login";
+    password = @"password";
+    
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Title" message:nil delegate:self cancelButtonTitle:@"Cancel" otherButtonTitles:@"Enter", nil];
+    alert.alertViewStyle = UIAlertViewStyleLoginAndPasswordInput;
+    alert.tag = 0;
+    [alert show];
+
     self.timer = [NSTimer scheduledTimerWithTimeInterval: 1.0 target:self   selector:@selector(updateScore) userInfo:nil repeats:YES];
 }
 
